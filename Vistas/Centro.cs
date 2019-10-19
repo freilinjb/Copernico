e9e156f0d1ba38a138a14c52e79b55e43f307e13
Freilin Jose <freilinjb@gmail.com>
2019-10-19 19:21:47 -0400

    Centro

---
+++ Vistas/Centro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vistas
+{
+    public partial class Centro : Form
+    {
+        private static Centro Instancia;
+
+        public static Centro ObtenerInstancia()
+        {
+            if (Instancia == null || Instancia.IsDisposed)
+                Instancia = new Centro();
+
+            Instancia.BringToFront();
+
+            return Instancia;
+        }
+        private Centro()
+        {
+            InitializeComponent();
+        }
+    }
+}
