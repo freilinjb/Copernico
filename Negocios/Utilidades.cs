b65f36776e312f846708cf75828114e728ddc770
Freilin Jose <freilinjb@gmail.com>
2019-10-19 19:34:18 -0400

    Organizacion de la Vista

--- Negocios/Utilidades.cs
+++ Negocios/Utilidades.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Negocios
 {
-    public class Class1
+    public class Utilidades
     {
     }
 }
--- Vistas/Centro.cs
+++ Vistas/Centro.cs
@@ -27,5 +27,5 @@ namespace Vistas
         {
             InitializeComponent();
         }
-    }
+    }   
 }
