f0df81bee61d44d87896b044d974f6c0165e3a5d
Freilin Jose <freilinjb@gmail.com>
2019-10-20 09:33:10 -0400

    Cellformat Y Fond en utilizades

--- Negocios/Utilidades.cs
+++ Negocios/Utilidades.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Negocios
 {
     public class Utilidades
     {
-
+        private static Font fuente = new Font(new FontFamily("Arial"), 11.0f);
+        public static void dataFormatoCelda(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            e.CellStyle.Font =  fuente;
+        }
+        public static void LimpiarDataGridView(DataGridView dataGrid)
+        {
+            if (dataGrid.Rows.Count > 0)
+            {
+                for (int i = dataGrid.Rows.Count - 1; i >= 0; i--)
+                {
+                    dataGrid.Rows.RemoveAt(i);
+                }
+            }
+        }
     }
 }
